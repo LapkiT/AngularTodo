3a9f586248aaa775fc395164dca192df53336f2d
LapkiT <gadzhibekovtimur@gmail.com>
2024-03-25 18:27:09 +0300

    adding pages component todo-list-app

---
+++ todo-list/src/app/pages/todo-list-app/todo-list-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-list-app.component.html',
+  styleUrl: './todo-list-app.component.scss'
+})
+export class TodoListAppComponent {
+
+}
