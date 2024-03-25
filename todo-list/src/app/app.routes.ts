a975964f196cdb26360355205e659997edd5099a
LapkiT <gadzhibekovtimur@gmail.com>
2024-03-26 01:25:25 +0300

    adding routes or routing

--- todo-list/src/app/app.routes.ts
+++ todo-list/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {TodoListAppComponent} from "./pages/todo-list-app/todo-list-app.component";
+import {IndexAuthPagesComponent} from "./Shared/Components/auth/index-auth/index-auth-pages.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'todo', component: TodoListAppComponent},
+  {path: '', component: IndexAuthPagesComponent}
+];
