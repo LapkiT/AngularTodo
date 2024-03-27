a40b00390643ca2fc27fa5955d1f9f72e0f68941
LapkiT <gadzhibekovtimur@gmail.com>
2024-03-27 10:17:50 +0300

    adding environments

--- todo-list/src/app/Shared/Components/auth/index-auth/index-auth-pages.component.ts
+++ todo-list/src/app/Shared/Components/auth/index-auth/index-auth-pages.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-index-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './index-auth-pages.component.html',
   styleUrl: './index-auth-pages.component.scss'
 })
 export class IndexAuthPagesComponent {
-
+  loginForm = new FormGroup({
+      email: new FormControl('jim@dundermifflin.com'),
+      password: new FormControl('password'),
+      fio: new FormControl('fio')
+    }
+  )
 }
---
+++ todo-list/src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  production: true,
+  backendOrigin: '/api.fit-meetups.ru',
+};
